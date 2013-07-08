9ead5b0da65ec3e6c3ff387f5c7ad41b114ebfb7
Thai Le <lnthai2002@gmail.com>
2013-07-08 14:39:13 -0400

    issue_60: expose :duration to public, fix validation on :month

--- app/models/financial/mortgage_adj.rb
+++ app/models/financial/mortgage_adj.rb
@@ -5,14 +5,15 @@ module Financial
     #month: selected month
     #amount: extra payment for the selected year
     #interest: new interest rate applied since the selected year
-    attr_accessible :month, :amount, :interest, :mortgage_id
+    attr_accessible :month, :amount, :interest, :duration, :mortgage_id
 
     belongs_to :mortgage
 
     validates_associated :mortgage
     validates :month, :presence => true
     validates :interest, :numericality => true , :allow_blank=>true
-    validates :month, :numericality => { :only_integer => true } , :allow_blank=>true
+    validates :month, :numericality => { :only_integer => true }
+    validates :duration, :numericality => { :only_integer => true } , :allow_blank=>true
     validate :rate_adj_must_be_on_first_month_of_year
 
     protected
