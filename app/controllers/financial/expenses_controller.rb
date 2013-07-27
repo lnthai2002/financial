41b545c32be0f14d6182bdd1314136bf71501b6a
Thai Le <lnthai2002@gmail.com>
2013-07-27 11:30:42 -0400

    issue_80: create should not redirect to show

--- app/controllers/financial/expenses_controller.rb
+++ app/controllers/financial/expenses_controller.rb
@@ -63,7 +63,7 @@ module Financial
   
       respond_to do |format|
         if @expense.save
-          format.html { redirect_to @expense, notice: 'Expense was successfully created.' }
+          format.html { redirect_to expenses_path, notice: 'Expense was successfully created.' }
           format.json { render json: @expense, status: :created, location: @expense }
         else
           format.html { render action: "new" }
@@ -79,7 +79,7 @@ module Financial
   
       respond_to do |format|
         if @expense.update_attributes(params[:expense])
-          format.html { redirect_to @expense, notice: 'Expense was successfully updated.' }
+          format.html { redirect_to expenses_path, notice: 'Expense was successfully updated.' }
           format.json { head :ok }
         else
           format.html { render action: "edit" }
