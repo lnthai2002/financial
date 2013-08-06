55f303cc85cc379079c695875b9b66e2e5767a0d
Thai Le <lnthai2002@gmail.com>
2013-08-05 21:23:08 -0400

    issue_82: recurring payment, the base of recuring income and recurring expense

---
+++ app/models/financial/recurring_payment.rb
@@ -0,0 +1,7 @@
+module Financial
+  class RecurringPayment < ActiveRecord::Base
+    monetize :amount_cents
+
+    attr_accessible :frequency, :first_date, :last_posted_date, :category_id, :amount, :note, :payment_type_id
+  end
+end
