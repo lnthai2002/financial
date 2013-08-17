26e812025a510c81de16d30843a4bae09a690d05
Thai Le <lnthai2002@gmail.com>
2013-08-17 10:00:44 -0400

    issue_93: make sure no one can assign non IncomeCategory to income, maybe redundant

--- app/models/financial/income.rb
+++ app/models/financial/income.rb
@@ -2,5 +2,14 @@ module Financial
   class Income < Payment
     belongs_to :income_category, :foreign_key => :category_id
     belongs_to :payment_type
+
+    validate :assoc_with_income_category
+
+    protected
+    def assoc_with_income_category
+      if self.income_category.class != Financial::IncomeCategory
+        errors.add(:income_category, "is invalid")
+      end
+    end
   end
 end
