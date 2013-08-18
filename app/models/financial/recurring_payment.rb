86f253d40362350532ea848336cfb743c418fe9c
Thai Le <lnthai2002@gmail.com>
2013-08-18 09:03:53 -0400

    issue_101: fix recurring payment cant be updated

--- app/models/financial/recurring_payment.rb
+++ app/models/financial/recurring_payment.rb
@@ -2,6 +2,6 @@ module Financial
   class RecurringPayment < ActiveRecord::Base
     monetize :amount_cents
 
-    attr_accessible :frequency, :first_date, :last_posted_date, :category_id, :amount, :note, :payment_type_id
+    attr_accessible :frequency, :first_date, :last_posted_date, :category_id, :amount, :note, :payment_type_id, :type
   end
 end
