424c47459a00a69824e8cf918e223ca5953a6857
Thai Le <lnthai2002@gmail.com>
2013-08-22 20:51:24 -0400

    issue_103: find as hash doesnt work

--- app/models/financial/income_category.rb
+++ app/models/financial/income_category.rb
@@ -1,5 +1,14 @@
 module Financial
   class IncomeCategory < Category
     has_many :incomes
+
+    def IncomeCategory.get_desc(type_id)
+      d = IncomeCategory.select("description").where(:id=>5).first
+      if !d.blank?
+        return d.description
+      else
+        return 'unknown'
+      end
+    end
   end
 end
