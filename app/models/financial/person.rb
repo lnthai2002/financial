6406a0e01e240e4a4ce2112c446e36f1f18474bb
Thai Le <lnthai2002@gmail.com>
2013-08-29 20:50:53 -0400

    issue_104: ability for owner

--- app/models/financial/person.rb
+++ app/models/financial/person.rb
@@ -1,10 +1,14 @@
 module Financial
   class Person < ActiveRecord::Base
-    set_table_name :users
+    self.table_name = 'users'
 
     attr_accessible :email
 
     has_one :finance, :dependent => :destroy
     has_many :plans, :dependent => :destroy
+    has_many :payments, :dependent => :destroy
+    has_many :incomes, :dependent => :destroy
+    has_many :expenses, :dependent => :destroy
+    has_many :recurring_payments, :dependent => :destroy
   end
 end
