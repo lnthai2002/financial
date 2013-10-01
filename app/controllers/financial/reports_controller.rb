680b2110f36efeffabd2a90d1f14dfad4b3ea489
Thai Le <lnthai2002@gmail.com>
2013-09-30 21:51:50 -0400

    issue_112: report controller

--- app/controllers/financial/reports_controller.rb
+++ app/controllers/financial/reports_controller.rb
@@ -10,41 +10,32 @@ module Financial
     set_tab :list, :expense, :only => %w(index)
 
     def index
+      prepare_report
+    end
+
+    #ajax call to update by_month
+    def by_month
+      prepare_report
+    end
+
+    #ajax call to update by_week
+    def by_week
+      prepare_report
+    end
+
+    #ajax call to update by_date
+    def by_date
+      prepare_report
+    end
+    protected
+
+    def prepare_report
       begin
         @date = Date.parse(params[:date])
       rescue
         @date = Date.today
       end
-      #inclusive search
-      @month_summary = summary_in_range(@date.beginning_of_month, @date.end_of_month)
-      @week_summary = summary_in_range(@date.beginning_of_week, @date.end_of_week)
-    end
-
-    private
-
-    def summary_in_range(start_date, end_date)
-      #inclusive search
-      conditions = ["pmt_date BETWEEN DATE(?) AND DATE(?)",
-                    start_date.strftime("%Y-%m-%d"),
-                    end_date.strftime("%Y-%m-%d")]
-
-      summary = {'expense'=>Expense.sum(:amount_cents, :conditions=>conditions, :group=>:category_id),
-                 'income'=>Income.sum(:amount_cents, :conditions=>conditions, :group=>:category_id)}
-
-      summary['total_expense'] = Money.new(0)
-      summary['expense'].each do |category_id, amount|
-        summary['expense'][category_id] = Money.new(amount)
-        summary['total_expense'] = summary['total_expense'] + summary['expense'][category_id]
-      end
-
-      summary['total_income'] = Money.new(0)
-      summary['income'].each do |category_id, amount|
-        summary['income'][category_id] = Money.new(amount)
-        summary['total_income'] = summary['total_income'] + summary['income'][category_id]
-      end
-      summary['total_balance'] = summary['total_income'] - summary['total_expense']
-
-      return summary
+      @report = Report.new(@date)
     end
   end
 end
