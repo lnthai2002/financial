6721d22b36bf1352c419c1f55ecd90476deaad53
Thai Le <lnthai2002@gmail.com>
2013-10-18 20:52:17 -0400

    issue_114: fix typo in migration for payee/payer

--- db/migrate/20131018021417_add_payee_payer_to_payment.rb
+++ db/migrate/20131018021417_add_payee_payer_to_payment.rb
@@ -1,5 +1,5 @@
 class AddPayeePayerToPayment < ActiveRecord::Migration
   def change
-    add_colum :financial_payments, :payee_payer, :string, :limit=>50
+    add_column :financial_payments, :payee_payer, :string, :limit=>50
   end
 end
--- db/migrate/20131018021429_add_payee_payer_to_recurring_payment.rb
+++ db/migrate/20131018021429_add_payee_payer_to_recurring_payment.rb
@@ -1,5 +1,5 @@
 class AddPayeePayerToRecurringPayment < ActiveRecord::Migration
   def change
-    add_colum :financial_recurring_payments, :payee_payer, :string, :limit=>50
+    add_column :financial_recurring_payments, :payee_payer, :string, :limit=>50
   end
 end
