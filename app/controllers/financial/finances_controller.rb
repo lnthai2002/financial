ade17459f61c41fbbf012033dd4eccd30ef4e71a
Thai Le <lnthai2002@gmail.com>
2016-07-16 11:45:26 -0400

    edit financial status

--- app/controllers/financial/finances_controller.rb
+++ app/controllers/financial/finances_controller.rb
@@ -5,9 +5,26 @@ module Financial
     def new
       person = Person.where(:email=>session[:cas_user]).first
       @finance = Finance.new(:person_id=>person.id)
+      render :form
     end
 
     def create
+      record_financial_status_of_current_user
+    end
+
+    def edit
+      person = Person.where(:email=>session[:cas_user]).first
+      @finance = person.finance
+      render :form
+    end
+
+    def update
+      record_financial_status_of_current_user
+    end
+
+    private
+
+    def record_financial_status_of_current_user
       person = Person.where(:email=>session[:cas_user]).first
       if person.finance.blank?
         @finance = Finance.new(finance_params)
@@ -19,11 +36,9 @@ module Financial
       if @finance.save
         redirect_to plans_path
       else
-        render :new
+        render :form
       end
     end
-
-    private
 
     def finance_params
       params.require(:finance).permit(:net_monthly_income, :net_monthly_expense)
